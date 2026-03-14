706d467aa8f8566dc96bb965ea8875f092e7bc54
Sami Daniel <samidanielpersonal@gmail.com>
2026-03-13 22:25:51 -0300

    Create initial model

---
+++ src/Embeddables/Password.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Embeddables;
+
+use Doctrine\ORM\Mapping\Embeddable;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[Embeddable]
+class Password {
+    #[ORM\Column(length: 72)]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 8, max: 72)]
+    #[Assert\Regex(pattern: '/[A-Z]/', message: 'Must have an uppercase letter.')]
+    #[Assert\Regex(pattern: '/[0-9]/', message: 'Must have a number.')]
+    #[Assert\Regex(pattern: '/^[A-Za-z0-9]+$/', message: 'Only letters and numbers are allowed.')]
+    private string $password;
+
+    public function setPassword(string $password): static {
+        $this->password = password_hash(trim($password), PASSWORD_BCRYPT);
+        return $this;
+    }
+
+    public function getPassword(): string {
+        return $this->password;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Embeddables\Password;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Embedded;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    private string $name;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Email(mode: 'strict')]
+    private string $email;
+
+    #[Embedded(class: Password::class, columnPrefix: false)]
+    private Password $password;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): Password
+    {
+        return $this->password;
+    }
+
+    public function setPassword(Password $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
